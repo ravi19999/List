45c328c0c2aa2311f18760c099b1d358a879c013
ravi19999 <dahalravindra@gmail.com>
2020-07-11 00:05:30 +0545

    Changed colors :lipstick: :sparkles:

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const util = require("util");
+const chalk = require("chalk");
 
 // const lstat = util.promisify(fs.lstat);
 
@@ -19,7 +20,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   for (let stats of allStats) {
     const index = allStats.indexOf(stats);
 
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(chalk.green(filenames[index]));
+    } else {
+      console.log(chalk.red(chalk.bold(filenames[index])));
+    }
   }
 });
 
