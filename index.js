9b3fa8dc0695a96a61b8e06e4b38742275c5c787
ravi19999 <dahalravindra@gmail.com>
2020-07-10 23:34:41 +0545

    Replaced promices with async/await :recycle: :poop:

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const util = require("util");
 // const lstat = util.promisify(fs.lstat);
 
 const { lstat } = fs.promises;
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
+  }
+
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
 
